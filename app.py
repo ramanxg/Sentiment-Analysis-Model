bf11dbb531c1aed9678f01b22a16ead12435bafb
Raman Gupta <ramanxg@gmail.com>
2019-08-05 22:06:54 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask
+import pickle
+from model import Model
+from flask_restful import reqparse, abort, Api, Resource
+import tweepy
+
+app = Flask(__name__)
+api = Api(app)
+
+CONSUMER_KEY = 'xtN9VUIDAyS5eoLC0gg035jQV'
+CONSUMER_SECRET = 'kXla9AOJebgLARddBBMMwsBcbewWoZIWApoYffUcSMBPVvlffK'
+ACCESS_TOKEN = '1053917274-KJg5VGA0JUki68TrWTuT5f54t83AhGseXKML1Jg'
+ACCESS_TOKEN_SECRET = 'ZJOhe3vk2x7145M641OIYUHHTEbi2LmMuIcLJNErH6VGD'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+twitter_api = tweepy.API(auth)
+
+def get_dataset(query):
+    results = twitter_api.search(query, count=100, lang='en')
+    texts = [tweet.text for tweet in results]
+    print(len(texts))
+    return texts
+
+model = Model()
+
+parser = reqparse.RequestParser()
+parser.add_argument('query')
+
+class Predict(Resource):
+    def get(self):
+        #parse arguments
+        args = parser.parse_args()
+        user_query = args['query']
+        
+        #get data from twitter
+        data = get_dataset(user_query)
+        
+        #process data and predict
+        sentences = model.process_text(data)
+        predictions = model.predict(sentences)
+
+        #calculate average of all predictions
+        average = sum(predictions) / len(predictions)
+        #generate text for prediction
+        prediction_text = model.process_prediction(float(average))
+
+        #create JSON
+        output = {'prediction': prediction_text, 'sentiment': average}
+
+        return output
+
+api.add_resource(Predict, '/')
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import pickle, re, nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.preprocessing import sequence
+import tweepy
+import numpy as np
+
+class Model:
+
+    def __init__(self):
+        self.model = load_model('sentimentmodel2.h5')
+        self.model._make_predict_function()
+        with open('tokenizer.pickle', 'rb') as handle:
+            self.tokenizer = pickle.load(handle)
+        self.max_sentence_length = self.model.layers[0].input_shape[1]
+        self.lemmatizer = WordNetLemmatizer()
+
+    def process_text(self, texts):
+        sentences = []
+        for s in texts:
+            replaced = re.sub(r'[^a-zA-z0-9\s]','', s.lower())
+
+            #tokenize words
+            words = word_tokenize(replaced)
+            
+            #lemmatize words
+            #lexicon = filtered_sentence
+            lexicon = [self.lemmatizer.lemmatize(i) for i in words]
+            sentences.append(lexicon)
+            
+        t = self.tokenizer.texts_to_sequences(sentences)
+        p =  sequence.pad_sequences(t, maxlen=self.max_sentence_length)
+        return p
+
+    def process_prediction(self, prediction):
+        if prediction <= 1:
+            return "Very Negative"
+        elif prediction <= 1.9:
+            return "Somewhat Negative"
+        elif 1.9 <= prediction <= 2.1:
+            return "Neutral"
+        elif prediction <= 3:
+            return "Somewhat Positive"
+        else:
+            return "Very Positive"
+
+    def predict(self, inputs):
+        return self.model.predict_classes(inputs)
+
